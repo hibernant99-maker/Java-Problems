977b9d2b18264dcd5913a8e8a5f26fb1784fe359
hibernant99-maker <hibernant99@gmail.com>
2026-08-09 19:45:11 +0600

    Add solution for ArrayStore.java

---
+++ ArrayStore.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ArrayStore {
+    public static void main(String[]args){
+        int[] array={1,2,3,4,5};
+
+
+int sum=0;
+for(int i=0;i<array.length;i++) {
+sum+=array[i];
+
+}
+System.out.println(sum);
+    }
+}
